a3f38a155702f3f6b595e7e58b76af6fe9bbb526
tintolee <63760590+tintolee@users.noreply.github.com>
2021-07-30 07:48:07 +0100

    commit message

--- EMR/settings.py
+++ EMR/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m5z3rphq-1mm#0xked)(fw!p-sokt_bh#9+a(=k*p41b44*adp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -42,11 +42,13 @@ INSTALLED_APPS = [
     'appointment',
     'user_profile',
     'bootstrap3',
-    'crispy_forms'
+    'crispy_forms',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -90,6 +92,9 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -138,3 +143,7 @@ STATICFILES_DIRS= [
     os.path.join(BASE_DIR,'static')
 ]
 STATIC_ROOT= os.path.join(BASE_DIR, 'assets')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
---
+++ Procfile
@@ -0,0 +1,1 @@
+"web: gunicorn EMR.wsgi" 
